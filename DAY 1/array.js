604852f1e4fa380d189835a11d876f8a2c24e277
Karan SD <karan.intern@edstechnologies.com>
2026-06-23 18:04:19 +0530

    arrays

--- DAY 1/array.js
+++ DAY 1/array.js
@@ -15,3 +15,15 @@ console.log(arr);
 arr.shift();
 console.log(arr); // remove the element from the first array 
 
+console.log(arr.length);
+
+console.log(arr.indexOf("hello")); // gives the index of the element
+
+
+/// Check the existence of that element in parameter 
+console.log(arr.includes(10)); // true
+console.log(arr.includes("karan"));// false 
+
+arr.sort()
+console.log(arr);
+
